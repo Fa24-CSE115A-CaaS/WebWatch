dc83aa533a2aee2b29842a13e8518d2f5a52af68
Cameron Candau <camcandau@gmail.com>
2024-10-15 20:34:53 -0700

    Tweaked directory structure

--- backend/main.py
+++
@@ -1,8 +0,0 @@
-import selenium
-
-print("hello WebWatch!!")
-
-try:
-    print("Selenium version: ", selenium.__version__)
-except NameError:
-    print("Selenium is not installed. Check DevContainer setup.")
