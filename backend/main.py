192fbe979c2ac98948347255349bc1ceacc471e7
Cameron Candau <camcandau@gmail.com>
2024-10-20 13:19:41 -0700

    main.py formatting

--- backend/main.py
+++ backend/main.py
@@ -12,33 +12,31 @@ def compare_texts(text1, text2):
     )
     return '\n'.join(diff)
 
-if __name__ == "__main__":
+# TODO: Define CLI (use argparse library)
+# Check # of arguments and if a URL is provided as an argument
+# Example: python3 main.py https://simonzhao.com
 
-    # TODO: Define CLI (use argparse library)
-    # Check # of arguments and if a URL is provided as an argument
-    # Example: python3 main.py https://simonzhao.com
+DELAY = 2 # in seconds
 
-    DELAY = 2 # in seconds
+with WebScraper() as scraper:
+    while True:
+        first = scraper.scrape_all_text("https://simonzhao.com")
+        print(f"First scrape completed. Waiting {DELAY} seconds before second scrape...")
+        # TODO: Look into more advanced time scheduling techniques (instead of sleep)
 
-    with WebScraper() as scraper:
-        while True:
-            first = scraper.scrape_all_text("https://simonzhao.com")
-            print(f"First scrape completed. Waiting {DELAY} seconds before second scrape...")
-            # TODO: Look into more advanced time scheduling techniques (instead of sleep)
+        time.sleep(DELAY)
 
-            time.sleep(DELAY)
+        #second = scraper.scrape_all_text("https://example.com")
+        second = scraper.scrape_all_text("https://simonzhao.com")
+        print("Second scrape completed.")
 
-            #second = scraper.scrape_all_text("https://example.com")
-            second = scraper.scrape_all_text("https://simonzhao.com")
-            print("Second scrape completed.")
-
-            if first is None or second is None:
-                print("An error occurred during scraping.")
-            elif first == second:
-                print("No change detected.")
-            else:
-                print("Change detected.")
-                diff = compare_texts(first, second)
-                print("Differences:")
-                print(diff)
-                break
+        if first is None or second is None:
+            print("An error occurred during scraping.")
+        elif first == second:
+            print("No change detected.")
+        else:
+            print("Change detected.")
+            diff = compare_texts(first, second)
+            print("Differences:")
+            print(diff)
+            break
