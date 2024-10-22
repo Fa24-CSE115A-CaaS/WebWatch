a702ee4d1372c6c89613160fa6a7e4e92809287c
Cameron Candau <camcandau@gmail.com>
2024-10-22 05:17:16 +0000

    Modified main.py to use asyncio.sleep to be non-blocking

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import time
 import difflib
+import asyncio
 from scrape import WebScraper
 
 def compare_texts(text1, text2):
@@ -16,27 +17,29 @@ def compare_texts(text1, text2):
 # Check # of arguments and if a URL is provided as an argument
 # Example: python3 main.py https://simonzhao.com
 
-DELAY = 2 # in seconds
+DELAY = 2  # in seconds
 
-with WebScraper() as scraper:
-    while True:
-        first = scraper.scrape_all_text("https://simonzhao.com")
-        print(f"First scrape completed. Waiting {DELAY} seconds before second scrape...")
-        # TODO: Look into more advanced time scheduling techniques (instead of sleep)
+async def main():
+    with WebScraper() as scraper:
+        while True:
+            first = scraper.scrape_all_text("https://simonzhao.com")
+            print(f"First scrape completed. Waiting {DELAY} seconds before second scrape...")
 
-        time.sleep(DELAY)
+            await asyncio.sleep(DELAY)
 
-        #second = scraper.scrape_all_text("https://example.com")
-        second = scraper.scrape_all_text("https://simonzhao.com")
-        print("Second scrape completed.")
+            second = scraper.scrape_all_text("https://simonzhao.com")
+            print("Second scrape completed.")
 
-        if first is None or second is None:
-            print("An error occurred during scraping.")
-        elif first == second:
-            print("No change detected.")
-        else:
-            print("Change detected.")
-            diff = compare_texts(first, second)
-            print("Differences:")
-            print(diff)
-            break
+            if first is None or second is None:
+                print("An error occurred during scraping.")
+            elif first == second:
+                print("No change detected.")
+            else:
+                print("Change detected.")
+                diff = compare_texts(first, second)
+                print("Differences:")
+                print(diff)
+                break
+
+if __name__ == "__main__":
+    asyncio.run(main())
