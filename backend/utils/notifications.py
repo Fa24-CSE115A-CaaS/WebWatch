5075326b4fb9216d8ad9b643844bca1c9566c2e0
Jordan Nguy <jrnguy@ucsc.edu>
2024-10-22 20:42:25 -0700

    Separated SMTP login from sender email

--- backend/utils/notifications.py
+++ backend/utils/notifications.py
@@ -1,20 +1,25 @@
 import os, smtplib, ssl, requests
 from dotenv import load_dotenv
+from email.utils import formataddr
 
 def send_mail(subject: str, message: str, recipients: list[str]):
     load_dotenv()
     
     smtp_server = os.getenv('EMAIL_SERVER')
-    smtp_port = os.getenv('EMAIL_PORT')
+    smtp_port = int(os.getenv('EMAIL_PORT'))
     sender_email = os.getenv('EMAIL_FROM')
+    username = os.getenv('EMAIL_USERNAME')
     password = os.getenv('EMAIL_PASSWORD')
 
+    formatted_sender = formataddr(('WebWatch', sender_email))
+
     msg = (f'Subject: {subject}\n\n{message}').encode('utf-8')
+    
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port=smtp_port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, recipients, msg)
+        server.login(username, password)
+        server.sendmail(formatted_sender, recipients, msg)
         
         
 def send_discord_msg(webhook_url, message):
