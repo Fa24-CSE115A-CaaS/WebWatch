4d948a8bc9951622aa3236c9d0015b3ac6a9b4a7
Jordan Nguy <jrnguy@ucsc.edu>
2024-10-22 20:55:30 -0700

    Replace SMTP payload with MIME

--- backend/utils/notifications.py
+++ backend/utils/notifications.py
@@ -1,6 +1,8 @@
 import os, smtplib, ssl, requests
 from dotenv import load_dotenv
 from email.utils import formataddr
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def send_mail(subject: str, message: str, recipients: list[str]):
     load_dotenv()
@@ -13,13 +15,21 @@ def send_mail(subject: str, message: str, recipients: list[str]):
 
     formatted_sender = formataddr(('WebWatch', sender_email))
 
-    msg = (f'Subject: {subject}\n\n{message}').encode('utf-8')
+    recipientsString = ""
+    for s in recipients:
+        recipientsString = recipientsString + s + " "
     
+    msg = MIMEMultipart()
+    msg['From'] = formatted_sender
+    msg['To'] = recipientsString
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port=smtp_port, context=context) as server:
         server.login(username, password)
-        server.sendmail(formatted_sender, recipients, msg)
+        server.sendmail(sender_email, recipientsString, msg.as_string())
         
         
 def send_discord_msg(webhook_url, message):
