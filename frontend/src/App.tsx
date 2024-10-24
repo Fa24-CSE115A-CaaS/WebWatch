a40e0a988d51b9cef37e2b47e7ec6153a396bc78
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-10-24 14:12:15 -0700

    chore(web app): setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,29 @@
 import { useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Components
+import Home from "./pages/Home";
+import Guide from "./pages/Guide";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/theme-guide",
+    Component: Guide,
+  },
+]);
+
+const App = () => {
   useEffect(() => {
     const theme = localStorage.getItem("theme");
-    console.log(theme);
     if (theme === "dark") {
       document.body.classList.add("dark");
     }
   }, []);
 
-  const handleThemeChange = () => {
-    if (document.body.classList.contains("dark")) {
-      document.body.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
-      document.body.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }
-  };
-
-  const btnStyles =
-    "text-text font-main m-3 block w-52 rounded-md px-3 py-2 font-medium border-[1px] border-border";
-  return (
-    <div className="bg-background h-screen w-screen p-5">
-      <div className="mx-auto w-min">
-        <button className={`${btnStyles} bg-primary`}>Primary</button>
-        <button className={`${btnStyles} bg-secondary`}>Secondary</button>
-        <button className={`${btnStyles} bg-accent text-text-contrast`}>
-          Accent
-        </button>
-        <button className={`${btnStyles} bg-accent-hover text-text-contrast`}>
-          Accent Hover
-        </button>
-        <button className={`${btnStyles} bg-border`}>Border</button>
-        <button className={`${btnStyles} bg-error`}>Error</button>
-        <button className={`${btnStyles} bg-info text-text-contrast`}>
-          Info
-        </button>
-        <button className={`${btnStyles} bg-text text-text-contrast`}>
-          Text
-        </button>
-        <button className={`${btnStyles} bg-text-contrast`}>
-          Text Contrast
-        </button>
-        <button
-          className={`${btnStyles} bg-primary`}
-          onClick={handleThemeChange}
-        >
-          Change Theme
-        </button>
-      </div>
-    </div>
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
