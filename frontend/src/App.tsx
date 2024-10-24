30a9dee267b73ceef23e87b7821d8d6d5882aaa2
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-10-24 14:00:27 -0700

    chore(web app): setup color themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,56 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    console.log(theme);
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    }
+  }, []);
 
+  const handleThemeChange = () => {
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
+
+  const btnStyles =
+    "text-text font-main m-3 block w-52 rounded-md px-3 py-2 font-medium border-[1px] border-border";
   return (
-    <button
-      className="mx-auto my-3 block cursor-pointer rounded-md bg-red-500 p-5 text-lg text-white
-        transition-all duration-100 ease-out hover:bg-red-700"
-      onClick={() => setCount((count) => count + 1)}
-    >
-      count is {count}
-    </button>
+    <div className="bg-background h-screen w-screen p-5">
+      <div className="mx-auto w-min">
+        <button className={`${btnStyles} bg-primary`}>Primary</button>
+        <button className={`${btnStyles} bg-secondary`}>Secondary</button>
+        <button className={`${btnStyles} bg-accent text-text-contrast`}>
+          Accent
+        </button>
+        <button className={`${btnStyles} bg-accent-hover text-text-contrast`}>
+          Accent Hover
+        </button>
+        <button className={`${btnStyles} bg-border`}>Border</button>
+        <button className={`${btnStyles} bg-error`}>Error</button>
+        <button className={`${btnStyles} bg-info text-text-contrast`}>
+          Info
+        </button>
+        <button className={`${btnStyles} bg-text text-text-contrast`}>
+          Text
+        </button>
+        <button className={`${btnStyles} bg-text-contrast`}>
+          Text Contrast
+        </button>
+        <button
+          className={`${btnStyles} bg-primary`}
+          onClick={handleThemeChange}
+        >
+          Change Theme
+        </button>
+      </div>
+    </div>
   );
 }
 
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,23 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["Space Grotesk", "Roboto", "sans-serif"],
+      },
+      colors: {
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        "accent-hover": "var(--accent-hover)",
+        border: "var(--border)",
+        error: "var(--error)",
+        info: "var(--info)",
+        text: "var(--text)",
+        "text-contrast": "var(--text-contrast)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
