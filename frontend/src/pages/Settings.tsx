59c1095ff77b3a20957520280dd58d5f32462459
Jordan Nguy <jrnguy@ucsc.edu>
2024-10-29 16:19:59 -0700

    Made recommendations #1,3,4,5 from Simon

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -8,7 +8,7 @@ const Settings = () => {
   };
 
   return (
-    <div className="w-2/3 mx-auto mt-8 rounded min-h-screen text-text">
+    <div className="w-1/2 mx-auto mt-8 rounded min-h-screen text-text">
       <div className="flex">
         <nav className="rounded-l-xl border-solid border-border border-2 min-h-screen w-1/3 min-w-fit bg-secondary text-left">
           {/* Column 1 - Sidebar*/}
@@ -86,25 +86,25 @@ const Settings = () => {
                   <label>Email</label>
                   <input
                     type="email"
-                    className="w-full rounded-lg border border-border bg-secondary p-2"
+                    className="w-full rounded-lg border border-border bg-secondary p-2 outline-none"
                   />
                   <h2 className="text-2xl my-8">Reset Password</h2>
                   <label>Password</label>
                   <input
                     type="password"
-                    className="w-full rounded-lg border border-border bg-secondary p-2"
+                    className="w-full rounded-lg border border-border bg-secondary p-2 outline-none"
                   />
                   <label>Confirm Password</label>
                   <input
                     type="password"
-                    className="w-full rounded-lg border border-border bg-secondary p-2"
+                    className="w-full rounded-lg border border-border bg-secondary p-2 outline-none"
                   />
                   <button className="mt-4 rounded-lg bg-accent p-2 px-16 text-text-contrast hover:bg-accent-hover">
                     Save
                   </button>
                 </form>
-                <h2 className="text-xl font-bold mt-8 mb-4">Danger Zone</h2>
-                <button className="rounded-lg bg-error p-2">
+                <h2 className="text-2xl my-8 mb-4">Danger Zone</h2>
+                <button className="rounded-lg bg-error px-3 py-2">
                   Delete my account
                 </button>
               </>
