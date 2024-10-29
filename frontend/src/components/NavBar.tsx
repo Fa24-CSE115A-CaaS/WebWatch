1d7f008a71cdac7f2b52c60243e51a8e05a1e6aa
Cameron Candau <camcandau@gmail.com>
2024-10-29 15:43:02 +0000

    Added links for Login and Register to NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -31,6 +31,12 @@ const NavBar = () => {
             </a>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center">
+            <a href="/login" className="mr-4">
+              Login
+            </a>
+            <a href="/register" className="mr-4">
+              Register
+            </a>
             <button
               className="relative flex rounded-full bg-gray-50 text-sm focus:outline-none focus:ring-2
                 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 dark:bg-gray-800"
