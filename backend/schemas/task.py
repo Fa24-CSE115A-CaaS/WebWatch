920d5b043d4a16e1b17378f436b89339459aa939
Cameron Candau <camcandau@gmail.com>
2024-11-15 06:16:30 +0000

    feat: Task.run() is now able to read/write from database for user information and updating task content

--- backend/schemas/task.py
+++ backend/schemas/task.py
@@ -2,12 +2,13 @@ from sqlalchemy import Column
 from typing import List, Literal
 from sqlalchemy.types import JSON
 from pydantic import field_validator
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Session, select
 import asyncio
+from schemas.user import User
+import os
 
 
 NotificationOptions = List[Literal["EMAIL", "DISCORD", "SLACK"]]
-
 
 class TaskBase(SQLModel):
     name: str = Field(max_length=50)
@@ -34,10 +35,17 @@ class TaskBase(SQLModel):
         return asyncio.run(self.run())
 
     async def run(self):
-        # PLACEHOLDER
+        # TODO: Implement scanning logic
+
+        loop = asyncio.get_event_loop()
+        user = await loop.run_in_executor(None, self.get_user)
+
         while self.enabled:
             await asyncio.sleep(1)
-            print(self.name)
+            print(f"User {user.email}: scan {self.name}")
+
+            # Proof of concept for concurrently updating content in db after scanning
+            # await loop.run_in_executor(None, self.update_content_in_db, "new content")
 
 
 class Task(TaskBase, table=True):
@@ -48,6 +56,23 @@ class Task(TaskBase, table=True):
         # Returns the task_id
         return self.id
 
+    def get_user(self):
+        from database import Database
+        db = Database(mode=os.getenv("ENV"))
+        session = next(db.get_session())
+        result = session.exec(select(User).where(User.id == self.user_id)).first()
+        return result
+
+    def update_content_in_db(self, new_content):
+        from database import Database
+        db = Database(mode=os.getenv("ENV"))
+        session = next(db.get_session())
+        task = session.exec(select(Task).where(Task.id == self.id)).first()
+        if task:
+            task.content = new_content
+            session.add(task)
+            session.commit()
+        session.close()
 
 class TaskGet(TaskBase):
     id: int
