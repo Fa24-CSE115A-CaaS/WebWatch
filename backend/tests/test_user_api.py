2844c655ee69e4ef85ca00d8253bf699a5af4caf
Jordan Nguy <jrnguy@ucsc.edu>
2024-11-17 16:38:32 -0800

    Create user api tests

---
+++ backend/tests/test_user_api.py
@@ -0,0 +1,97 @@
+from fastapi.testclient import TestClient
+from server import app
+import argon2
+
+client = TestClient(app)
+
+""" Removed until user delete is implemented
+def test_user_register_success():
+    # Test user registration
+    response = client.post("/users/register", json={"email": "test@webwatch.live", "password": "Password1233!"})
+    assert response.status_code == 201
+"""
+
+def test_user_register_fail_duplicate():
+    # Test user registration with duplicate username
+    response = client.post("/users/register", json={"email": "test@webwatch.live", "password": "Password1233!!!"})
+    assert response.status_code == 409
+
+def test_user_login_fail():
+    # Test user login with wrong password
+    try:
+        response = client.post(
+            "/users/login",
+            data={
+                "grant_type": "password",
+                "username": "test@webwatch.live",
+                "password": "WrongPassword1233!!",
+                "scope": "",
+                "client_id": "string",
+                "client_secret": "string"
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"}
+        )
+        assert response.status_code == 400
+    except argon2.exceptions.VerifyMismatchError:
+        pass  # This is expected
+    else:
+        assert False, "Expected VerifyMismatchError was not raised"
+
+def test_user_login_success():
+    # Test user login with correct password
+    response = client.post(
+        "/users/login",
+        data={
+            "grant_type": "password",
+            "username": "test@webwatch.live",
+            "password": "Password1233!",
+            "scope": "",
+            "client_id": "string",
+            "client_secret": "string"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+    assert response.status_code == 200
+
+def test_user_verify():
+    # Test user verification
+    response = client.post(
+        "/users/login",
+        data={
+            "grant_type": "password",
+            "username": "test@webwatch.live",
+            "password": "Password1233!",
+            "scope": "",
+            "client_id": "string",
+            "client_secret": "string"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+    assert response.status_code == 200
+    access_token = response.json()["access_token"]
+
+    response = client.get(f"/users/verify?token={access_token}")
+    assert response.status_code == 200
+    assert response.json() == {"email": "test@webwatch.live"}
+
+"""
+def test_user_logout():
+    response = client.post(
+        "/users/login",
+        data={
+            "grant_type": "password",
+            "username": "test@webwatch.live",
+            "password": "Password1233!",
+            "scope": "",
+            "client_id": "string",
+            "client_secret": "string"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+    assert response.status_code == 200
+    
+    access_token = response.json()["access_token"]
+    response = client.post("/users/logout", json={"email": "test@webwatch.live", "token": access_token})
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+"""
