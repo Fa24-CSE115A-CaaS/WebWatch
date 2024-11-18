c4044d2aeed68d859bce538128206b2557052261
Jordan Nguy <jrnguy@ucsc.edu>
2024-11-17 17:40:12 -0800

    Updated to use bearer tokens

--- backend/tests/test_task_api.py
+++ backend/tests/test_task_api.py
@@ -3,18 +3,26 @@ from server import app
 
 client = TestClient(app)
 
+def register_user():
+    response = client.post("/users/register", json={"email": "test@webwatch.live", "password": "Password1233!"})
+    assert response.status_code == 201
+    return response.json()["access_token"]
+
+access_token = register_user()
+
 def test_task_retrieve_list():
     # Test task list retrieval
-    response = client.get("/tasks")
+    response = client.get("/tasks", headers={"Authorization": "Bearer " + access_token})
     assert response.status_code == 200
 
 def test_task_create():
     # Test task creation
     response = client.post(
         "/tasks",
+        headers={"Authorization": "Bearer " + access_token},
         json={
             "name": "Test Task",
-            "content": "This is a test task content",
+            "content": "",
             "url": "https://webwatch.live/test",
             "discord_url": "string",
             "enabled_notification_options": ["EMAIL"],
