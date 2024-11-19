fe71ae346f14172d3054940af6ac5efa42ead279
Cameron Candau <camcandau@gmail.com>
2024-11-19 07:07:38 +0000

    fix: improve robustness of chromedriver install script (tested)

--- backend/.devcontainer/post-create-commands.sh
+++ backend/.devcontainer/post-create-commands.sh
@@ -11,7 +11,7 @@ apt-get update
 apt-get install -y libnss3 libdbus-1-3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
 
 # Download Chrome
-cd scraper-linux64 && ./downloadchrome.sh
+cd scraper-linux64 && bash ./downloadchrome.sh
 
 cd /workspaces/WebWatch/backend
 
